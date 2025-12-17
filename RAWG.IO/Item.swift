c7d8ee5f3d6b07102e78aae4345b477132e93f30
Finn Christoffer <finn.christoffer@gmail.com>
2025-12-17 23:11:51 +0700

    feat(project): setup initial project

---
+++ RAWG.IO/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RAWG.IO
+//
+//  Created by Finn Christoffer on 17/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}

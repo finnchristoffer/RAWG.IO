875aee549ba5b7af024755e4f57e33f6ed047fbb
Finn Christoffer <finn.christoffer@gmail.com>
2025-12-18 10:43:23 +0700

    build(tuist): add Tuist configuration with Xcode 16 and Swift 6 support

---
+++ Tuist/Config.swift
@@ -0,0 +1,9 @@
+import ProjectDescription
+
+let config = Config(
+    compatibleXcodeVersions: .upToNextMajor("16.0"),
+    swiftVersion: "6.0",
+    generationOptions: .options(
+        enforceExplicitDependencies: true
+    )
+)
